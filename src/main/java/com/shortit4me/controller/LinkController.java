c6683334945eb0cbe008d1e03c6877e06d786f77
vasiliyChobot <i@vchobot.ru>
2023-01-17 13:38:57 +0300

    refactoring all

--- src/main/java/com/shortit4me/controller/LinkController.java
+++ src/main/java/com/shortit4me/controller/LinkController.java
@@ -6,6 +6,7 @@ import com.shortit4me.repository.LongLinkRepository;
 import com.shortit4me.repository.ShortLinkRepository;
 import com.shortit4me.service.LongLinkService;
 import com.shortit4me.utils.ShortLinkGenerator;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -39,8 +41,8 @@ public class LinkController {
         return new ModelAndView("home");
     }
 
-    @PostMapping
-    //@Transactional
+    /*@PostMapping
+    @Transactional
     public String addLinks(@RequestParam("link") String link, Model model) {
         LongLink longLink = new LongLink();
         longLink.setUserLink(link);
@@ -50,18 +52,27 @@ public class LinkController {
         shortLink.setId(longLink.getId());
         shortLinkRepository.save(shortLink);
         return "redirect:/";
+    }*/
+
+    @PostMapping
+    @Transactional
+    public String addLinks(@RequestParam("link") String link, Model model) {
+        LongLink longLink = new LongLink();
+        longLink.setUserLink(link);
+        longLinkService.saveNewLongLink(longLink);
+        ShortLink shortLink = new ShortLink();
+        shortLink.setGeneratedLink(shortLinkGenerator.generateShortLink());
+        shortLink.setId(longLink.getId());
+        shortLinkRepository.save(shortLink);
+        //model.addAttribute("shortUrl", shortLink.getGeneratedLink());
+        return "redirect:/showShortUrl";
     }
 
     @GetMapping(SHORT_LINK)
     public String getLongLink(@PathVariable final String shortLink) {
 
         List<ShortLink> shortLinkList = shortLinkRepository.findAll();
-        String id = "";
-        for(var link : shortLinkList) {
-            if (link.getGeneratedLink().equals(shortLink)) {
-                id = String.valueOf(link.getId());
-            }
-        }
-        return longLinkRepository.findById(id).get().getUserLink();
+        val linkFromRepo = shortLinkRepository.findByGeneratedLink(shortLink).get().getId();
+        return String.valueOf(longLinkRepository.findById(String.valueOf(linkFromRepo)));
     }
 }
--- src/main/java/com/shortit4me/repository/ShortLinkRepository.java
+++ src/main/java/com/shortit4me/repository/ShortLinkRepository.java
@@ -3,6 +3,8 @@ package com.shortit4me.repository;
 import com.shortit4me.model.ShortLink;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ShortLinkRepository extends JpaRepository<ShortLink, String> {
+import java.util.Optional;
 
+public interface ShortLinkRepository extends JpaRepository<ShortLink, String> {
+    Optional<ShortLink> findByGeneratedLink(String generatedLink);
 }
