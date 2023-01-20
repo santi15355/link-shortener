02e04368289b004cd900dccaf95a18aac9d6faab
Vasiliy Chobot <83598552+santi15355@users.noreply.github.com>
2023-01-20 03:40:27 +0300

    adding buttons, fixing migrations, replacing H2 DB with Postgres

--- src/main/java/com/shortit4me/model/LongLink.java
+++ src/main/java/com/shortit4me/model/LongLink.java
@@ -1,21 +1,22 @@
 package com.shortit4me.model;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.Temporal;
+
 import java.util.Date;
 
-import static javax.persistence.TemporalType.TIMESTAMP;
+import static jakarta.persistence.TemporalType.TIMESTAMP;
 
 @Entity
 @Getter
@@ -30,10 +31,6 @@ public class LongLink {
     private long id;
 
     private String userLink;
-
-
-    /*@OneToOne
-    private ShortLink shortLink;*/
 
     @CreationTimestamp
     @Temporal(TIMESTAMP)
--- src/main/java/com/shortit4me/model/ShortLink.java
+++ src/main/java/com/shortit4me/model/ShortLink.java
@@ -1,15 +1,14 @@
 package com.shortit4me.model;
 
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 
 @Entity
 @Getter
@@ -23,8 +22,5 @@ public class ShortLink {
     private long id;
 
     private String generatedLink;
-
-    /*@OneToOne
-    private LongLink longLink;*/
 
 }
